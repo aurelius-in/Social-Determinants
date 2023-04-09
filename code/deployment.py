4f0b2f131af4e1c2cf3e0b91bde9e3e905e1e816
Oliver Ellison, MS SD <89275354+aurelius-in@users.noreply.github.com>
2023-04-08 19:54:26 -0500

    consolidated portfolio assets

--- code/deployment.py
+++ code/deployment.py
@@ -2,6 +2,16 @@ import pandas as pd
 from joblib import dump, load
 from sklearn.pipeline import Pipeline
 
+# If a pre-trained model has already been developed and saved to a file using the joblib 
+# library this code will load the pre-trained model using the load function from the joblib library. 
+# We then define a pre-processing pipeline that includes an imputer to handle missing values and a 
+# scaler to standardize the data. We apply this pre-processing pipeline to the input data using the 
+# fit_transform method. We then make predictions using the pre-trained model by calling the predict 
+# method on the transformed data. Finally, we save the predictions to a CSV file with the patient IDs 
+# and their corresponding predictions. You can modify this implementation based on the specific 
+# requirements of your project, such as using a different pre-processing pipeline or outputting 
+# predictions in a different format.
+
 def deploy_model(data, model_path):
     # Load pre-trained model
     model = load(model_path)
